b2e35412f0c9d880812642114de4a43472bb325a
WallXiaoming <47529387+WallXiaoming@users.noreply.github.com>
2020-09-21 18:46:48 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,29 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        wuKong.setServoAngel(wuKong.ServoList.S0, 0)
-        wuKong.setServoAngel(wuKong.ServoList.S1, 0)
-        wuKong.setServoAngel(wuKong.ServoList.S2, 0)
-        wuKong.setServoAngel(wuKong.ServoList.S4, 0)
+        wuKong.mecanumRun(wuKong.RunList.Front, 35)
     }
     if (receivedNumber == 2) {
-        wuKong.setServoAngel(wuKong.ServoList.S0, 180)
-        wuKong.setServoAngel(wuKong.ServoList.S1, 180)
-        wuKong.setServoAngel(wuKong.ServoList.S2, 180)
-        wuKong.setServoAngel(wuKong.ServoList.S4, 180)
+        wuKong.mecanumStop()
     }
     if (receivedNumber == 3) {
-        wuKong.setServoAngel(wuKong.ServoList.S0, 90)
-        wuKong.setServoAngel(wuKong.ServoList.S1, 90)
-        wuKong.setServoAngel(wuKong.ServoList.S2, 90)
-        wuKong.setServoAngel(wuKong.ServoList.S4, 90)
+        wuKong.mecanumSpin(wuKong.TurnList.Left, 20)
     }
 })
-let 指南针角度 = 0
 radio.setGroup(0)
+wuKong.mecanumWheel(
+wuKong.ServoList.S1,
+wuKong.ServoList.S2,
+wuKong.ServoList.S3,
+wuKong.ServoList.S4
+)
 basic.forever(function () {
-    指南针角度 = input.rotation(Rotation.Pitch)
-    if (指南针角度 < 45) {
+    if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(1)
-    } else if (指南针角度 < 135) {
+    }
+    if (input.buttonIsPressed(Button.B)) {
         radio.sendNumber(2)
-    } else {
+    }
+    if (input.buttonIsPressed(Button.AB)) {
         radio.sendNumber(3)
     }
-    basic.showNumber(指南针角度)
 })
